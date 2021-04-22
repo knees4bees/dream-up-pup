c9fed46df432ff36c7d69b4289d5db78baf5c06a
Katie Bee <knees4bees@gmail.com>
2021-04-22 16:11:38 -0600

    Add write story button to landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Landing.css';
 
 const Landing = ({ breeds }) => {
@@ -7,11 +8,16 @@ const Landing = ({ breeds }) => {
 
   return (
     <main className="landing">
-      <h2 className="landing__sentence">I want to write a story about </h2>
-      <select className="landing__menu">
+    <div className="landing__sentence">
+      <h2 className="landing__sentence--words">I want to write a story about </h2>
+      <select className="landing__sentence--menu">
         {listItems}
       </select>
-      <h2 className="landing__sentence"> dogs</h2>
+      <h2 className="landing__sentence--words"> dogs</h2>
+    </div>
+    <Link to="/create" className="landing__button--container">
+      <button className="landing__button--button">Write story</button>
+    </Link>
     </main>
   )
 }
