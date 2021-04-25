eeb32dc2c78525b95efa2e82b375099e9927175a
Katie Bee <knees4bees@gmail.com>
2021-04-25 12:16:28 -0600

    Add tests for landing page

--- cypress/integration/dream_up_pup_spec.js
+++ cypress/integration/dream_up_pup_spec.js
@@ -12,7 +12,30 @@ describe('Dream Up Pup -- landing page', () => {
   it('Should see a navigation bar', () => {
     cy.get('.nav-bar')
       .contains('Dream Up Pup')
-      .get('img')
-      .should('have.class', 'nav-bar__home--img')
+      .get('.nav-bar__home--img').should('have.attr', 'alt', 'doghouse')
+      .get('.nav-bar__shelf--img').should('have.attr', 'alt', 'three books on a shelf')
+  });
+
+  it('Should have a sentence with nested dropdown', () => {
+    cy.get('.landing__sentence')
+      .contains('I want to write a story about')
+      .get('select').should('have.value', 'Affenpinscher')
+      .get('select').should('not.have.value', 'Beagle')
+  });
+
+  it('Should have a button to kick off story writing', () => {
+    cy.get('button')
+      .contains('Write story')
+  });
+
+  it('Should be able to select a different breed', () => {
+    cy.get('select').select('Beagle').should('have.value', 'Beagle')
+    cy.get('select').select('Boxer').should('have.value', 'Boxer')
+    cy.get('select').select('Beagle').should('not.have.value', 'Boxer')
+  });
+
+  it('Should take user to a new URL when button is clicked', () => {
+    cy.get('button').click()
+      .location('pathname').should('eq', '/create')
   });
 });
