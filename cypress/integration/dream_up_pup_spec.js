2c86738a0ddd883362b570f55e06287a2606e9d9
Katie Bee <knees4bees@gmail.com>
2021-04-25 11:29:58 -0600

    Add fixture and first test for landing page

--- cypress/integration/dream_up_pup_spec.js
+++ cypress/integration/dream_up_pup_spec.js
@@ -1,5 +1,18 @@
-describe('Dream Up Pup', () => {
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+describe('Dream Up Pup -- landing page', () => {
+  beforeEach(() => {
+    cy.fixture('mockBreeds').then(( data ) => {
+      cy.intercept('https://dog.ceo/api/breeds/list/all', {
+      statusCode: 200,
+      body:  data
+      })
+    })
+    cy.visit('http://localhost:3000/')
+  });
+
+  it('Should see a navigation bar', () => {
+    cy.get('.nav-bar')
+      .contains('Dream Up Pup')
+      .get('img')
+      .should('have.class', 'nav-bar__home--img')
   });
 });
